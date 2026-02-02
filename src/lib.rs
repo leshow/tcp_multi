98048a88367d5e3457de9acc902807ecdaa00541
Evan Cameron <cameron.evan@gmail.com>
2026-02-01 22:10:46 -0500

    initial

---
+++ src/lib.rs
@@ -0,0 +1,501 @@
+pub const BUF_SIZE: usize = 4096;
+
+use std::{
+    collections::HashMap,
+    mem,
+    net::SocketAddr,
+    os::fd::{AsRawFd, FromRawFd, IntoRawFd},
+    sync::{
+        Arc, Mutex,
+        atomic::{AtomicBool, AtomicU16, Ordering},
+    },
+    time::Instant,
+};
+
+use anyhow::Result;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpSocket, TcpStream},
+    sync::{mpsc, oneshot},
+    task::JoinSet,
+};
+// use tokio_util::{bytes::Bytes, task::TaskTracker};
+use tracing::{debug, warn};
+
+use crate::msg::SerialMsg;
+
+mod msg;
+
+#[derive(Debug)]
+pub struct DnsQuery<T> {
+    pub to_send: T,
+    pub reply: oneshot::Sender<T>,
+}
+
+#[derive(Debug)]
+pub struct PendingSend<T> {
+    pub to_send: T,
+    pub next_id: u16,
+    pub reply: oneshot::Sender<T>,
+}
+
+impl<T> PendingSend<T> {
+    pub fn new(to_send: T, next_id: u16, reply: oneshot::Sender<T>) -> Self {
+        Self {
+            to_send,
+            reply,
+            next_id,
+        }
+    }
+}
+
+const MAX_ID: u16 = u16::MAX - 1;
+
+#[derive(Debug)]
+pub struct PendingResponse<T> {
+    pub original_id: u16,
+    pub reply: oneshot::Sender<T>,
+    pub sent_at: Instant,
+}
+
+#[derive(Debug)]
+pub struct TcpConnection {
+    pending: ResponseMap<PendingResponse<SerialMsg>>,
+    queued_tx: mpsc::UnboundedSender<PendingSend<SerialMsg>>,
+    next_id: Arc<AtomicU16>,
+    is_closing: Arc<AtomicBool>,
+    // last_activity: Arc<Mutex<Instant>>,
+    max_in_flight: Option<usize>,
+    tasks: JoinSet<Result<()>>,
+}
+
+type ResponseMap<T> = Arc<Mutex<HashMap<u16, T>>>;
+
+impl TcpConnection {
+    pub async fn new(addr: SocketAddr, max_in_flight: Option<usize>) -> Result<Arc<Self>> {
+        let pending = Arc::new(Mutex::new(HashMap::new()));
+        let (read, send) = tcpstream_connect(addr).await?.into_split();
+        let (queued_tx, queued_rx) = mpsc::unbounded_channel();
+
+        let is_closing = Arc::new(AtomicBool::new(false));
+        // let last_activity = Arc::new(Instant::now());
+        let mut this = Self {
+            pending: pending.clone(),
+            tasks: JoinSet::new(),
+            queued_tx,
+            next_id: Arc::new(AtomicU16::new(0)),
+            is_closing: is_closing.clone(),
+            // last_activity: last_activity.clone(),
+            max_in_flight,
+        };
+        this.tasks.spawn(read_half(
+            read,
+            addr,
+            is_closing,
+            // last_activity,
+            pending.clone(),
+        ));
+        this.tasks.spawn(send_half(send, queued_rx, pending));
+
+        Ok(Arc::new(this))
+    }
+
+    // copied from new for testing
+    // New generic function to handle both real streams and test streams
+    #[cfg(test)]
+    fn from_split<R, W>(
+        read: R,
+        send: W,
+        addr: SocketAddr,
+        max_in_flight: Option<usize>,
+    ) -> Result<Arc<Self>>
+    where
+        R: AsyncReadExt + Unpin + Send + 'static,
+        W: AsyncWriteExt + Unpin + Send + 'static,
+    {
+        let pending = Arc::new(Mutex::new(HashMap::new()));
+        let (queued_tx, queued_rx) = mpsc::unbounded_channel();
+
+        let is_closing = Arc::new(AtomicBool::new(false));
+        let mut this = Self {
+            pending: pending.clone(),
+            tasks: JoinSet::new(),
+            queued_tx,
+            next_id: Arc::new(AtomicU16::new(0)),
+            is_closing: is_closing.clone(),
+            max_in_flight,
+        };
+        this.tasks
+            .spawn(read_half(read, addr, is_closing, pending.clone()));
+        this.tasks.spawn(send_half(send, queued_rx, pending));
+
+        Ok(Arc::new(this))
+    }
+
+    // @TODO can be async if switch to bounded chan
+    pub fn send(&self, query: DnsQuery<SerialMsg>) -> Result<()> {
+        // should this be done in the pool and not on send?
+        if !self.is_healthy() || self.queued_tx.is_closed() {
+            self.set_closing();
+            return Err(anyhow::Error::msg("connection unhealthy"));
+        }
+        if query.to_send.bytes().len() < 12 {
+            // notify err? msg too small
+            return Err(anyhow::Error::msg("message too small"));
+        }
+        let next_id = self.next_id.fetch_add(1, Ordering::Release);
+        if next_id >= MAX_ID {
+            self.set_closing();
+        }
+        let DnsQuery { to_send, reply } = query;
+        // note: there is a small race here where we could have reached the max_in_flight
+        // but still send because the send_half has not added to the pending map yet
+
+        if let Err(err) = self.queued_tx.send(PendingSend {
+            to_send,
+            next_id,
+            reply,
+        }) {
+            warn!(%err, "TCP pending queue dropped, setting connection to closing");
+            self.set_closing();
+        }
+        Ok(())
+    }
+    fn set_closing(&self) {
+        self.is_closing.swap(true, Ordering::Relaxed);
+    }
+    fn is_closing(&self) -> bool {
+        self.is_closing.load(Ordering::Relaxed)
+    }
+    pub fn is_healthy(&self) -> bool {
+        if self.is_closing() {
+            return false;
+        }
+        if self.reached_max_id() {
+            return false;
+        }
+        if let Some(max) = self.max_in_flight {
+            let pending = { self.pending.lock().unwrap().len() };
+            if pending >= max {
+                return false;
+            }
+        }
+        self.is_usable()
+    }
+    fn reached_max_id(&self) -> bool {
+        self.next_id.load(Ordering::Relaxed) >= MAX_ID
+    }
+    pub fn is_usable(&self) -> bool {
+        // @TODO peek
+        true
+    }
+    pub fn is_idle(&self) -> bool {
+        self.pending.lock().unwrap().is_empty()
+    }
+}
+
+async fn read_half<R: AsyncReadExt + Unpin>(
+    mut recv: R,
+    addr: SocketAddr,
+    is_closing: Arc<AtomicBool>,
+    pending: ResponseMap<PendingResponse<SerialMsg>>,
+) -> Result<()> {
+    loop {
+        match SerialMsg::read(&mut recv, addr).await {
+            Ok(mut msg) => {
+                // Extract DNS ID from response header (first 2 bytes)
+                if msg.bytes().len() < 12 {
+                    // notify_io_error
+                    continue;
+                }
+                // @TODO set last activity?
+                let resp_id = msg.msg_id();
+                let (r, is_empty) = {
+                    let mut lock = pending.lock().unwrap();
+
+                    let entry = lock.remove(&resp_id);
+                    let is_empty = lock.is_empty();
+                    (entry, is_empty)
+                };
+                if let Some(resp) = r {
+                    let latency = resp.sent_at.elapsed();
+                    // restore original id
+                    msg.replace_id(u16::to_be_bytes(resp.original_id));
+
+                    if let Err(err) = resp.reply.send(msg) {
+                        warn!(
+                            id = err.msg_id(),
+                            ?latency,
+                            "TCP: sending over oneshot failed (likely reason: got response back for an already dropped message)"
+                        )
+                    }
+                }
+                // if there's nothing to read and we're closing
+                if is_empty && is_closing.load(Ordering::Relaxed) {
+                    break;
+                }
+            }
+            Err(err) => {
+                warn!(%err, "tcp read half error");
+                // notify_io_error
+                is_closing.swap(true, Ordering::Relaxed);
+                break;
+            }
+        };
+    }
+    // Notify all pending requests of the error
+    let mut pending = pending.lock().unwrap();
+    for (_id, resp) in pending.drain() {
+        // Just drop the sender, which will signal an error to the receiver
+        drop(resp.reply);
+    }
+    Ok(())
+}
+
+async fn send_half<W: AsyncWriteExt + Unpin>(
+    mut conn: W,
+    mut queued_msgs: mpsc::UnboundedReceiver<PendingSend<SerialMsg>>,
+    pending: ResponseMap<PendingResponse<SerialMsg>>,
+) -> Result<()> {
+    while let Some(msg) = queued_msgs.recv().await {
+        let PendingSend {
+            mut to_send,
+            reply,
+            next_id,
+        } = msg;
+        // swap id
+        let original_id = to_send.msg_id();
+        to_send.replace_id(u16::to_be_bytes(next_id));
+
+        match to_send.write(&mut conn).await {
+            Ok(_) => {
+                {
+                    let mut lock = pending.lock().unwrap();
+                    // insert next_id and for recv
+                    lock.insert(
+                        next_id,
+                        PendingResponse {
+                            original_id,
+                            reply,
+                            sent_at: Instant::now(),
+                        },
+                    );
+                    drop(lock);
+                }
+                if let Err(err) = conn.flush().await {
+                    warn!(%err, "TCP flush failed");
+                    // notify_io_error?
+                    break;
+                }
+            }
+            Err(err) => {
+                // notify_io_error
+                warn!(%err, "send half error tcp");
+                break;
+            }
+        }
+    }
+    debug!("TCP send half exited");
+    // will drop queued_msgs and Sender will fail
+    Ok(())
+}
+
+async fn tcpstream_connect(addr: SocketAddr) -> Result<TcpStream> {
+    let tfo_on = 1;
+    let soc = socket2::Socket::new(
+        if addr.is_ipv4() {
+            socket2::Domain::IPV4
+        } else {
+            socket2::Domain::IPV6
+        },
+        socket2::Type::STREAM,
+        None,
+    )?;
+    // soc.set_reuse_port(true)?;
+    soc.set_nonblocking(true)?;
+    soc.set_tcp_nodelay(true)?;
+    set_socket_option(&soc, libc::IPPROTO_TCP, libc::TCP_FASTOPEN_CONNECT, tfo_on)?;
+
+    let soc = unsafe { TcpSocket::from_raw_fd(soc.into_raw_fd()) };
+    Ok(soc.connect(addr).await?)
+}
+
+fn set_socket_option<Fd: AsRawFd>(
+    socket: &Fd,
+    level: libc::c_int,
+    name: libc::c_int,
+    value: libc::c_int,
+) -> Result<(), std::io::Error> {
+    let rc = unsafe {
+        libc::setsockopt(
+            socket.as_raw_fd(),
+            level,
+            name,
+            &value as *const _ as _,
+            mem::size_of_val(&value) as _,
+        )
+    };
+
+    if rc != -1 {
+        Ok(())
+    } else {
+        Err(std::io::Error::last_os_error())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hickory_proto::{
+        op::{Message, MessageType, OpCode, Query},
+        rr::{Name, RecordType},
+    };
+    use std::str::FromStr;
+    use std::time::Duration;
+    use tokio::io::{AsyncReadExt, split};
+
+    // Helper to create a mock DNS message using hickory-proto
+    fn create_hickory_msg(id: u16, name: &str) -> Message {
+        let name = Name::from_str(name).unwrap();
+        let query = Query::query(name, RecordType::A);
+        let mut message = Message::new();
+        message
+            .set_id(id)
+            .set_message_type(MessageType::Query)
+            .set_op_code(OpCode::Query)
+            .add_query(query)
+            .set_recursion_desired(true);
+        message
+    }
+
+    async fn setup_test_conn(
+        max_flight: Option<usize>,
+    ) -> (Arc<TcpConnection>, tokio::io::DuplexStream, SocketAddr) {
+        let (client, server) = tokio::io::duplex(BUF_SIZE);
+        let (read, write) = split(client);
+        let addr = "127.0.0.1:53".parse().unwrap();
+        let conn = TcpConnection::from_split(read, write, addr, max_flight).unwrap();
+        (conn, server, addr)
+    }
+
+    #[tokio::test]
+    async fn test_send_and_receive_ok() {
+        let (conn, mut server, addr) = setup_test_conn(None).await;
+
+        // Test server task that mimics a DNS server
+        tokio::spawn(async move {
+            // Read the length prefix
+            let mut len_buf = [0u8; 2];
+            server.read_exact(&mut len_buf).await.unwrap();
+            let len = u16::from_be_bytes(len_buf) as usize;
+
+            // Read the actual message
+            let mut msg_buf = vec![0u8; len];
+            server.read_exact(&mut msg_buf).await.unwrap();
+
+            // The multiplexer should have replaced the original ID with its own internal ID.
+            // Let's check that the received ID is 0 (the first one used).
+            let received_id = u16::from_be_bytes(msg_buf[0..2].try_into().unwrap());
+            assert_eq!(received_id, 0);
+
+            // Create a response message, using the internal ID
+            let mut response = create_hickory_msg(received_id, "example.com.");
+            response.set_message_type(MessageType::Response);
+            let serial_response = SerialMsg::from_message(&response, addr).unwrap();
+
+            // Send the response back
+            serial_response.write(&mut server).await.unwrap();
+        });
+
+        let (tx, rx) = oneshot::channel();
+        let original_id = 1234;
+        let query_msg = create_hickory_msg(original_id, "example.com.");
+        let query = DnsQuery {
+            to_send: SerialMsg::from_message(&query_msg, addr).unwrap(),
+            reply: tx,
+        };
+
+        conn.send(query).unwrap();
+
+        let response = tokio::time::timeout(Duration::from_secs(1), rx)
+            .await
+            .expect("should get a response")
+            .unwrap();
+
+        // Check that the original ID was restored by the connection manager
+        assert_eq!(response.msg_id(), original_id);
+    }
+
+    #[tokio::test]
+    async fn test_max_in_flight() {
+        let (conn, _server, addr) = setup_test_conn(Some(1)).await;
+
+        let (tx, _rx) = oneshot::channel();
+        let query_msg = create_hickory_msg(1, "query1.com.");
+        let query = DnsQuery {
+            to_send: SerialMsg::from_message(&query_msg, addr).unwrap(),
+            reply: tx,
+        };
+
+        // First send should be ok
+        assert!(conn.is_healthy());
+        conn.send(query).unwrap();
+
+        // Connection should now be unhealthy due to max_in_flight
+        assert!(!conn.is_healthy());
+
+        // Second send should fail
+        let (tx2, _rx2) = oneshot::channel();
+        let query_msg2 = create_hickory_msg(2, "query2.com.");
+        let query2 = DnsQuery {
+            to_send: SerialMsg::from_message(&query_msg2, addr).unwrap(),
+            reply: tx2,
+        };
+        assert!(conn.send(query2).is_err());
+    }
+
+    #[tokio::test]
+    async fn test_connection_closes_on_read_error() {
+        let (conn, server, _) = setup_test_conn(None).await;
+        assert!(conn.is_healthy());
+
+        // Drop the server end to cause a read error in the connection's read_half
+        drop(server);
+
+        // Wait a moment for the read_half task to detect the error and set is_closing
+        tokio::time::sleep(Duration::from_millis(20)).await;
+
+        assert!(!conn.is_healthy());
+        assert!(conn.is_closing());
+    }
+
+    #[tokio::test]
+    async fn test_connection_closes_on_send_error() {
+        let (conn, server, addr) = setup_test_conn(None).await;
+        assert!(conn.is_healthy());
+
+        // Drop the server end to cause a write error
+        drop(server);
+
+        let (tx, rx) = oneshot::channel();
+        let query_msg = create_hickory_msg(1, "query1.com.");
+        let query = DnsQuery {
+            to_send: SerialMsg::from_message(&query_msg, addr).unwrap(),
+            reply: tx,
+        };
+
+        // This send might succeed in queueing, but the send_half task will fail to write.
+        // The error on send will drop the oneshot sender in PendingSend.
+        let _ = conn.send(query);
+
+        // The receiver should get an error because the sender was dropped.
+        let result = rx.await;
+        assert!(result.is_err());
+
+        // Wait a moment for the tasks to process the error and update state
+        tokio::time::sleep(Duration::from_millis(20)).await;
+
+        assert!(!conn.is_healthy());
+        assert!(conn.is_closing());
+    }
+}
---
+++ src/msg.rs
@@ -0,0 +1,154 @@
+use hickory_proto::{
+    ProtoError,
+    op::{Header, Message, Query},
+    serialize::binary::{BinDecodable, BinDecoder},
+};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::UdpSocket,
+};
+
+use std::{borrow::Borrow, io, net::SocketAddr};
+
+// use crate::udp::UdpRecv;
+
+/// A message pulled from TCP or UDP and serialized to bytes, stored with a
+/// [`SocketAddr`]
+///
+/// [`SocketAddr`]: std::net::SocketAddr
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SerialMsg {
+    message: Vec<u8>,
+    addr: SocketAddr,
+}
+
+impl SerialMsg {
+    /// Construct a new `SerialMsg` and the source or destination address
+    pub fn new(message: Vec<u8>, addr: SocketAddr) -> Self {
+        SerialMsg { message, addr }
+    }
+
+    /// Constructs a new `SerialMsg` from another `SerialMsg` and a `SocketAddr`
+    pub fn from_message(msg: &Message, addr: SocketAddr) -> io::Result<Self> {
+        Ok(SerialMsg {
+            message: msg.to_vec()?,
+            addr,
+        })
+    }
+
+    /// Get a reference to the bytes
+    pub fn bytes(&self) -> &[u8] {
+        &self.message
+    }
+
+    /// Get a mutable reference to the bytes
+    pub fn bytes_mut(&mut self) -> &mut [u8] {
+        &mut self.message
+    }
+
+    /// Get the source or destination address (context dependent)
+    pub fn addr(&self) -> SocketAddr {
+        self.addr
+    }
+
+    /// Set the source or destination address
+    pub fn set_addr(&mut self, addr: SocketAddr) {
+        self.addr = addr;
+    }
+
+    /// Gets the bytes and address as a tuple
+    pub fn contents(self) -> (Vec<u8>, SocketAddr) {
+        (self.message, self.addr)
+    }
+
+    /// Deserializes the inner data into a Message
+    pub fn to_message(&self) -> io::Result<Message> {
+        Ok(Message::from_vec(&self.message)?)
+    }
+
+    /// Get dns message id from the buffer
+    pub fn msg_id(&self) -> u16 {
+        u16::from_be_bytes([self.message[0], self.message[1]])
+    }
+
+    /// Replace current dns message id (first 2 bytes) with a new id
+    pub fn replace_id(&mut self, new_id: [u8; 2]) {
+        self.message[0..2].copy_from_slice(&new_id)
+    }
+
+    /// Get the response code from the byte buffer (not using edns)
+    pub fn r_code(&self) -> u8 {
+        let qr_to_rcode = u8::from_be_bytes([self.message[3]]);
+        0x0F & qr_to_rcode
+    }
+
+    /// Decode the first 12 bytes of `message` into a [`Header`].
+    /// A decoded DNS query header.
+    ///
+    /// [`Header`]: hickory_proto::op::Header
+    pub fn to_header(&self) -> io::Result<Header> {
+        let mut decoder = BinDecoder::new(self.bytes());
+        Ok(Header::read(&mut decoder)?)
+    }
+
+    /// Truncate the bytes to the specified size
+    pub fn truncate_bytes(&mut self, size: usize) {
+        self.message.truncate(size);
+        self.set_truncated();
+    }
+
+    /// Set the TC bit, to indicate the message was truncated
+    pub fn set_truncated(&mut self) {
+        self.message[2] |= 0b0000_0010;
+    }
+
+    /// Build the Query from this msg
+    pub fn query(&self) -> Result<Query, ProtoError> {
+        Query::from_bytes(&self.bytes()[12..])
+    }
+
+    /// create a new `SerialMsg` from any `AsyncReadExt`
+    ///
+    /// Cancel-safety: not cancel-safe, uses an internal stack buffer & `read_exact`
+    pub async fn read<S>(stream: &mut S, src: SocketAddr) -> io::Result<Self>
+    where
+        S: Unpin + AsyncReadExt,
+    {
+        let mut buf = [0u8; 2];
+        let _bytes_len = stream.read_exact(&mut buf).await?;
+        let len = u16::from_be_bytes(buf) as usize;
+
+        let mut buf = vec![0; len];
+        stream.read_exact(&mut buf).await?;
+
+        Ok(SerialMsg::new(buf, src))
+    }
+
+    /// write a `SerialMsg` to any `AsyncWriteExt`
+    ///
+    /// Cancel-safety: not cancel-safe. `write_all` uses internal state
+    pub async fn write<S>(&self, stream: &mut S) -> io::Result<usize>
+    where
+        S: Unpin + AsyncWriteExt,
+    {
+        let len = self.message.len();
+        let byte_len = (len as u16).to_be_bytes();
+        // send
+        stream.write_all(&byte_len).await?;
+        stream.write_all(&self.message).await?;
+
+        Ok(len)
+    }
+
+    /// Receive a `SerialMsg` from any new `UdpRecv`
+    ///
+    /// Cancel-safety: not cancel-safe, uses an internal stack buffer
+    pub async fn recv<S>(stream: &S) -> io::Result<Self>
+    where
+        S: Borrow<UdpSocket>,
+    {
+        let mut buf = [0u8; crate::BUF_SIZE];
+        let (len, src) = stream.borrow().recv_from(&mut buf).await?;
+        Ok(SerialMsg::new(buf[..len].to_vec(), src))
+    }
+}
